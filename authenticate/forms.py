e2f718cc78627a0725cb5da21dc1f16bf7ccce21
Ankush Rayabhari <ankush.rayabhari@gmail.com>
2015-09-10 18:29:57 -0700

    deleted customer form

--- authenticate/forms.py
+++ authenticate/forms.py
@@ -2,10 +2,6 @@ from django import forms
 from .models import Customer
 from django.contrib.auth.models import User
 
-class CustomerRegisterForm(forms.ModelForm):
-	class Meta:
-		model = Customer
-
 class UserRegisterForm(forms.ModelForm):
 	password = forms.CharField(widget=forms.PasswordInput())
 
